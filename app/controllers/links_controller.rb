bb8b2844a494b4da2d15b9241d801c566323c360
richard bluestein <richard@bluestein.com>
2013-10-08 01:15:43 -0500

    links written to dbase

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,11 +3,21 @@ class LinksController < ApplicationController
 		puts params
 		@original_link = params[:link][:original_link]
 		@shortened_link = generate_shortlink
+		link = Link.new(original_link: @original_link, shortened_link: @shortened_link, counter: 0)
+		link.save
+		if session[:id]
+			User.find(session[:id]).links << link
+		end
 	end
 
 	def show
 		puts params.inspect
 	end
+
+
+
+
+
 
 	def generate_shortlink(length=6)
 	  chars = 'abcdefghijklmnopqrstuvwxyzabABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
